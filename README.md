0cf1b78bd7d567291928d474e89822b2479d4f36
yinyong02 <yinyong02@baidu.com>
2016-06-24 15:56:29 +0800

    test passed

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # panto-transformer-write
-
-[![NPM version][npm-image]][npm-url] [![Downloads][downloads-image]][npm-url] [![Dependency status][david-dm-image]][david-dm-url] [![Dev Dependency status][david-dm-dev-image]][david-dm-dev-url]
+[![NPM version][npm-image]][npm-url] [![Downloads][downloads-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency status][david-dm-image]][david-dm-url] [![Dev Dependency status][david-dm-dev-image]][david-dm-dev-url]
 
 Write transformer for panto.
 
@@ -8,9 +7,7 @@ Write transformer for panto.
 panto.loadTransformer('write');
 
 panto.pick('**/*.js').pipe(panto.read()).pipe(panto.write({
-    destname: (file) => {
-        return file.filename;
-    }
+    destname: file => file.filename
 })).end();
 ```
 
@@ -20,6 +17,8 @@ panto.pick('**/*.js').pipe(panto.read()).pipe(panto.write({
 [npm-url]: https://npmjs.org/package/panto-transformer-write
 [downloads-image]: http://img.shields.io/npm/dm/panto-transformer-write.svg
 [npm-image]: http://img.shields.io/npm/v/panto-transformer-write.svg
+[travis-url]: https://travis-ci.org/pantojs/panto-transformer-write
+[travis-image]: http://img.shields.io/travis/pantojs/panto-transformer-write.svg
 [david-dm-url]:https://david-dm.org/pantojs/panto-transformer-write
 [david-dm-image]:https://david-dm.org/pantojs/panto-transformer-write.svg
 [david-dm-dev-url]:https://david-dm.org/pantojs/panto-transformer-write#info=devDependencies
---
+++ test/test.js
@@ -0,0 +1,89 @@
+/**
+ * Copyright (C) 2016 pantojs.xyz
+ * test.js
+ *
+ * changelog
+ * 2016-06-24[15:46:45]:revised
+ *
+ * @author yanni4night@gmail.com
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+'use strict';
+const fs = require('fs');
+const assert = require('assert');
+const panto = require('panto');
+const WriteTransformer = require('../');
+
+describe('panto-transformer-write', () => {
+    describe('.destname', () => {
+        it('should support function', done => {
+            const file = {
+                filename: '1.js',
+                destname: '2.js',
+                content: '0x0810'
+            };
+
+            panto.setOptions({
+                cwd: __dirname,
+                output: 'output'
+            });
+
+            new WriteTransformer({
+                destname: file => file.destname
+            }).transform(file).then(tfile => {
+                const path = __dirname + '/output/2.js';
+                assert.ok(fs.existsSync(path));
+                assert.ok(fs.statSync(path).isFile());
+                const content = fs.readFileSync(path, 'utf8');
+                assert.deepEqual(content, '0x0810');
+            }).then(() => {
+                done();
+            });
+        });
+    });
+    describe('#transform', () => {
+        it('should write', done => {
+            const file = {
+                filename: 'a.js',
+                content: '0x0810'
+            };
+
+            panto.setOptions({
+                cwd: __dirname,
+                output: 'output'
+            });
+
+            new WriteTransformer({
+                destname: 'a.js'
+            }).transform(file).then(tfile => {
+                const path = __dirname + '/output/a.js';
+                assert.ok(fs.existsSync(path));
+                assert.ok(fs.statSync(path).isFile());
+                const content = fs.readFileSync(path, 'utf8');
+                assert.deepEqual(content, '0x0810');
+            }).then(() => {
+                done();
+            });
+        });
+        it('should not write if content is null/undefined', () => {
+            const file = {
+                filename: 'm.js'
+            };
+
+            panto.setOptions({
+                cwd: __dirname,
+                output: 'output'
+            });
+
+            new WriteTransformer({
+                destname: 'n.js'
+            }).transform(file).then(tfile => {
+                const path = __dirname + '/output/n.js';
+                assert.ok(!fs.existsSync(path));
+            }).then(() => {
+                done();
+            });
+        });
+    });
+});
